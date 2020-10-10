76ec43760660d6f9dadf122b51a0cf4e6b1d5191
Charlotte Trible <ctrble@gmail.com>
2020-10-10 10:38:17 -0500

    setting up spawners and lsystem

---
+++ Sprouts/Assets/Scripts/LSystem.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+// https://www.youtube.com/watch?v=wo3VGyF5z8Q
+public class LSystem : MonoBehaviour {
+  public int iterations;
+  public float angle;
+  public float width;
+  public float minLeafLength;
+  public float maxLeafLength;
+  public float minBranchLength;
+  public float maxBranchLength;
+  public float variance;
+
+  public GameObject tree;
+  public GameObject branch;
+  public GameObject leaf;
+
+  private const string axiom = "X";
+
+  private Dictionary<char, string> rules = new Dictionary<char, string>();
+  private Stack<SavedTransform> savedTransforms = new Stack<SavedTransform>();
+  private Vector3 initialPosition;
+
+  private string currentPath = "";
+  [SerializeField]
+  private float[] randomRotations;
+
+  void Awake() {
+    randomRotations = new float[1000];
+    for (int i = 0; i < randomRotations.Length; i++) {
+      randomRotations[i] = Random.Range(-1f, 1f);
+    }
+
+    rules.Add('X', "[-FX][+FX][FX]");
+    rules.Add('F', "FF");
+
+    // Generate();
+  }
+
+  public void Generate() {
+    currentPath = axiom;
+
+    StringBuilder stringBuilder = new StringBuilder();
+
+    for (int i = 0; i < iterations; i++) {
+      char[] currentPathChars = currentPath.ToCharArray();
+      for (int j = 0; j < currentPathChars.Length; j++) {
+        stringBuilder.Append(rules.ContainsKey(currentPathChars[j]) ? rules[currentPathChars[j]] : currentPathChars[j].ToString());
+      }
+
+      currentPath = stringBuilder.ToString();
+      stringBuilder = new StringBuilder();
+    }
+
+    for (int k = 0; k < currentPath.Length; k++) {
+      switch (currentPath[k]) {
+        case 'F':
+          initialPosition = transform.position;
+          bool isLeaf = false;
+
+          GameObject currentElement;
+          if (currentPath[k + 1] % currentPath.Length == 'X' || currentPath[k + 3] % currentPath.Length == 'F' && currentPath[k + 4] % currentPath.Length == 'X') {
+            currentElement = Instantiate(leaf, transform.position, transform.rotation);
+            isLeaf = true;
+          }
+          else {
+            currentElement = Instantiate(branch, transform.position, transform.rotation);
+          }
+
+          currentElement.transform.SetParent(tree.transform);
+
+          TreeElement currentTreeElement = currentElement.GetComponent<TreeElement>();
+
+          float length = 0f;
+          if (isLeaf) {
+            length = UnityEngine.Random.Range(minLeafLength, maxLeafLength);
+            transform.Translate(Vector3.up * 1f * length);
+            // transform.Translate(Vector3.up * 2f * Random.Range(minLeafLength, maxLeafLength));
+          }
+          else {
+            length = UnityEngine.Random.Range(minBranchLength, maxBranchLength);
+            transform.Translate(Vector3.up * 1f * length);
+            // transform.Translate(Vector3.up * 2f * Random.Range(minBranchLength, maxBranchLength));
+          }
+
+          // currentTreeElement.lineRenderer.startWidth = width;
+          // currentTreeElement.lineRenderer.endWidth = width;
+          currentTreeElement.lineRenderer.startWidth = currentTreeElement.lineRenderer.startWidth * width;
+          currentTreeElement.lineRenderer.endWidth = currentTreeElement.lineRenderer.endWidth * width;
+          currentTreeElement.lineRenderer.SetPosition(1, new Vector3(0, length, 0));
+          // currentElement.transform.rotation = transform.rotation;
+          currentTreeElement.lineRenderer.sharedMaterial = currentTreeElement.material;
+          break;
+
+        case 'X':
+          break;
+
+        case '+':
+          transform.Rotate(Vector3.forward * angle * (1f + variance / 100f * randomRotations[k % randomRotations.Length]));
+          break;
+
+        case '-':
+          transform.Rotate(Vector3.back * angle * (1f + variance / 100f * randomRotations[k % randomRotations.Length]));
+          break;
+
+        case '*':
+          transform.Rotate(Vector3.up * 120f * (1f + variance / 100f * randomRotations[k % randomRotations.Length]));
+          break;
+
+        case '/':
+          transform.Rotate(Vector3.down * 120f * (1f + variance / 100f * randomRotations[k % randomRotations.Length]));
+          break;
+
+        case '[':
+          savedTransforms.Push(new SavedTransform() {
+            position = transform.position,
+            rotation = transform.rotation
+          });
+          break;
+
+        case ']':
+          SavedTransform stack = savedTransforms.Pop();
+
+          transform.position = stack.position;
+          transform.rotation = stack.rotation;
+          break;
+      }
+    }
+  }
+}
---
+++ Sprouts/Assets/Scripts/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+  public LSystem lSystem;
+  public GameObject treePrefab;
+
+  void Start() {
+    GameObject tree = Instantiate(treePrefab);
+    lSystem.tree = tree;
+    lSystem.Generate();
+  }
+}
