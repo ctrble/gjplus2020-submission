532e2fa21a89370c11536de0d0c7f95d706d9a3c
Charlotte Trible <ctrble@gmail.com>
2020-10-10 20:55:47 -0500

    adding rotation to the world

--- Sprouts/Assets/Scripts/Gardener.cs
+++ Sprouts/Assets/Scripts/Gardener.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class Gardener : MonoBehaviour {
   public LSystem lSystem;
+  public int numberOfSeeds;
   public Vector3[] seeds;
+  public Transform world;
   public GameObject treePrefab;
 
   void Start() {
+    seeds = new Vector3[numberOfSeeds];
+
     for (int i = 0; i < seeds.Length; i++) {
-      // if (i > 0) {
-      //   return;
-      // }
+      seeds[i] = Random.onUnitSphere * (world.localScale.x * 0.5f);
+
       TreeRoot tree = Instantiate(treePrefab).GetComponent<TreeRoot>();
 
       tree.PlantTree(lSystem);
       tree.transform.position = seeds[i];
+      tree.transform.up = seeds[i] - Vector3.zero;
     }
   }
 }
---
+++ Sprouts/Assets/Scripts/MouseDragRotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDragRotate : MonoBehaviour {
+
+  private Vector3 mousePreviousPosition = Vector3.zero;
+  private Vector3 mousePositionDelta = Vector3.zero;
+  private Camera cameraMain;
+
+  void Start() {
+    cameraMain = Camera.main;
+    transform.up = cameraMain.transform.up;
+    transform.right = cameraMain.transform.right;
+  }
+
+  void Update() {
+    if (Input.GetMouseButton(0)) {
+      mousePositionDelta = Input.mousePosition - mousePreviousPosition;
+
+      transform.Rotate(cameraMain.transform.up, -Vector3.Dot(mousePositionDelta, cameraMain.transform.right), Space.World);
+      transform.Rotate(cameraMain.transform.right, Vector3.Dot(mousePositionDelta, cameraMain.transform.up), Space.World);
+    }
+
+    mousePreviousPosition = Input.mousePosition;
+  }
+}
